cf39ec92898c07618e8e5b000c0d463fce186c0b
ykup <julia.kuptsova@simbirsoft.com>
2015-09-02 10:24:39 +0300

    module events implementation

--- lib/accessfilemanager.php
+++ lib/accessfilemanager.php
@@ -9,21 +9,31 @@ namespace Notamedia\Niceaccess;
 class AccessFileManager
 {
     protected $path;
+    protected $fileContent;
 
     /**
      * @param string $path Full path to file .access.php
+     * @param string $fileContent Content of file
      */
-    public function __construct($path)
+    public function __construct($path, $fileContent)
     {
         $this->path = $path;
+        $this->fileContent = $fileContent;
     }
 
     /**
      * Resave file .access.php, with usage symbol of the codes users groups instead id's
      */
-    public function resave()
+    public function replaceFileAccessContent()
     {
-        // @todo Пересохраняем файл .access.php, подменяя в нём айдишники групп пользователей
-        // @todo на метод \Bex\Tools\Groups::getId('code')
+        $fileName = '.access.php';
+        if(strlen($this->path) >= strlen($fileName) && substr($this->path, -strlen($fileName)) == $fileName)
+        {
+            // @todo Пересохраняем файл .access.php, подменяя в нём айдишники групп пользователей
+            // @todo на метод \Bex\Tools\Groups::getId('code')
+            return $this->fileContent;
+        }
+
+        return $this->fileContent;
     }
 }
--- lib/eventmanager.php
+++ lib/eventmanager.php
@@ -6,20 +6,40 @@
 
 namespace Notamedia\Niceaccess;
 
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
 class EventManager
 {
-    public static function onChangePermissions($path, $permission)
+    public static function onBeforeChangeFile($abs_path, &$strContent)
     {
-        // @todo Вызываем \Notamedia\Niceaccess\AccessFileManager, что бы пересохранить .access.php
+        $accessFileManager = new AccessFileManager($abs_path, $strContent);
+
+        $strContent = $accessFileManager->replaceFileAccessContent();
+
+        return true;
     }
 
     public static function onBeforeGroupAdd(&$fields)
     {
-        // @todo Запрещаем сохранять группу без символьного кода
+        global $APPLICATION;
+
+        if(empty($fields['STRING_ID']))
+        {
+            $APPLICATION->ThrowException(Loc::getMessage('NOTA_NICEACCESS_GROUP_CODE_ERROR_MESSAGE'));
+            return false;
+        }
     }
 
     public static function onBeforeGroupUpdate($id, &$fields)
     {
-        // @todo Запрещаем сохранять группу без символьного кода
+        global $APPLICATION;
+
+        if(empty($fields['STRING_ID']))
+        {
+            $APPLICATION->ThrowException(Loc::getMessage('NOTA_NICEACCESS_GROUP_CODE_ERROR_MESSAGE'));
+            return false;
+        }
     }
 }
